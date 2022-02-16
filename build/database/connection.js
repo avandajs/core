6e216ffd74107d87319d04bb91f3ff20787ae710
Sulaiman Adewale <hackerslord96@gmail.com>
2022-02-16 03:00:21 +0100

    build fix

--- build/database/connection.js
+++ build/database/connection.js
@@ -5,11 +5,11 @@ const sequelize_1 = require("sequelize");
 let connection;
 async function default_1(config) {
     if (connection) {
-        console.log('COnnecttion:>>', ' Using old');
         return connection;
     }
     connection = new sequelize_1.Sequelize(config.dbName, config.dbUser, config.dbPassword, {
         host: process.env.DB_HOST,
+        port: 3307,
         dialect: config.dbDialect
     });
     try {
--- configs/dbConfig.ts
+++ configs/dbConfig.ts
@@ -2,7 +2,8 @@ type dbConfig = {
     dbName: string,
     dbUser: string,
     dbPassword: string,
-    dbDialect: 'mysql' | 'mariadb' | 'postgres' | 'mssql'
+    dbDialect: 'mysql' | 'mariadb' | 'postgres' | 'mssql',
+    socketPath?: string
 }
 
 export default dbConfig
