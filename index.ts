737ec3848db89bd04bf044b43a655127d3020abb
Sulaiman Adewale <hackerslord96@gmail.com>
2022-02-24 03:14:33 +0100

    Added SMS module

--- index.ts
+++ index.ts
@@ -2,13 +2,12 @@ import Connection from "./database/connection";
 import dbConfig from "./configs/dbConfig";
 import * as Validator from "./modules/Validator";
 import * as Mail from "./modules/Mailer/Mail";
-import SMS from "./modules/SMS/Sms";
+import * as SMS from "./modules/SMS/Sms";
 import * as Token from "./modules/Token";
 import * as Hash from "./modules/Hash";
 import serverConfig from "./configs/serverConfig";
 import * as Env from "./modules/Env"
 import * as App from "./modules/App"
-
 
 export {
     Connection,
